cf7e709010a67f3e82a0f916a62085ef6d1ca2bd
domna <domnakanari@gmail.com>
2020-03-04 19:54:48 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3000/admin',
+  firebase: {
+    apiKey: 'AIzaSyC_JSBdXEjBOZkqtWVWSaUkvq1vDC6Cvvw',
+    authDomain: 'eshopdomna.firebaseapp.com',
+    databaseURL: 'https://eshopdomna.firebaseio.com',
+    projectId: 'eshopdomna',
+    storageBucket: 'eshopdomna.appspot.com',
+    messagingSenderId: '538144558738',
+    appId: '1:538144558738:web:5a95c9d73b2ae5a692e04b',
+    measurementId: 'G-7LPYBGZG94'
+  }
 };
